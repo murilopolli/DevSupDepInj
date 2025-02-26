a5596eacefaf6c10bcf7096c4d9a7962b31c6ce6
Murilo Polli <murilopolli@gmail.com>
2025-02-25 22:27:32 -0300

    first commit

---
+++ src/main/java/copm/devsup/depinj/DepinjApplication.java
@@ -0,0 +1,13 @@
+package copm.devsup.depinj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepinjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepinjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/copm/devsup/depinj/DepinjApplicationTests.java
@@ -0,0 +1,13 @@
+package copm.devsup.depinj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepinjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
